a74bac212fa4a33dd4b4565114c082b9469ce0eb
Mohamed Eladly <msrtv365@yahoo.com>
2023-08-20 19:39:17 +0300

    Fix: handle %b

--- print_numbers.c
+++ print_numbers.c
@@ -1,40 +1,6 @@
 #include "main.h"
 
 /**
- * itos - convert an int to a string
- * @n: unsigned integer
- *
- * Return: string rep of integer
- */
-char *itos(unsigned int n)
-{
-	int dig_count = 0;
-	int idx, num = n;
-	char *str;
-
-	/* Count the digits of n*/
-	do {
-		dig_count++;
-		num /= 10;
-	} while (num != 0);
-
-	/* Allocate space for all digits + null byte*/
-	str = malloc(dig_count + 1);
-	if (!str)
-		return (NULL);
-	/*Fill the string strating from the last character*/
-	idx = dig_count - 1;
-	do {
-		str[idx] = '0' + (n % 10);
-		n /= 10;
-		idx--;
-	} while (n != 0);
-	str[dig_count] = '\0';
-
-	return (str);
-}
-
-/**
  * print_int - prints an integer to stdout.
  * @args: integer
  *
@@ -76,21 +42,11 @@ int print_int(va_list args)
  **/
 int print_binary(va_list args)
 {
-	int arr[5000], i, len = 0;
+	int binary;
 	unsigned int num;
 
 	num = va_arg(args, unsigned int);
-	if (!num)
-		return (0);
-	for (i = 0; num > 0; i++)
-	{
-		arr[i] = num % 2;
-		num = num / 2;
-	}
-	for (i = i - 1; i >= 0; i--)
-	{
-		_printf("%d", arr[i]);
-		len++;
-	}
-	return (len);
+
+	binary = itob(num);
+	return (_printf("%d", binary));
 }
